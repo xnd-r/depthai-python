73c19400a71b696e4f9e949b6cc2a2c55d468fe3
iamsg <>
2020-08-23 20:31:02 +0300

    fix ownership of python numpy array

--- host/py_module/py_bindings.cpp
+++ host/py_module/py_bindings.cpp
@@ -993,8 +993,8 @@ PYBIND11_MODULE(depthai, m)
 
     // for NNET_PACKET in nnet_packets:
     py::class_<NNetPacket, std::shared_ptr<NNetPacket>>(m, "NNetPacket")
-        .def("get_tensor", &NNetPacket::getTensor, py::return_value_policy::copy)
-        .def("get_tensor", &NNetPacket::getTensorByName, py::return_value_policy::copy)
+        .def("get_tensor", &NNetPacket::getTensor, py::return_value_policy::take_ownership)
+        .def("get_tensor", &NNetPacket::getTensorByName, py::return_value_policy::take_ownership)
         .def("getMetadata", &NNetPacket::getMetadata, py::return_value_policy::copy)
         .def("getOutputsList", &NNetPacket::getOutputsList, py::return_value_policy::copy)     
         .def("getOutputsDict", &NNetPacket::getOutputsDict, py::return_value_policy::copy)     
--- shared/version.hpp
+++ shared/version.hpp
@@ -1,4 +1,4 @@
 #pragma once
  
-const char *c_depthai_dev_version = "012efe7af81d4d730288193dd93412c9ec12e5a1";
+const char *c_depthai_dev_version = "63c2799d093c3c6ad9c81bf1ffc9ae1a6e75c83b";
 const char *c_depthai_version = "0.1.0";
